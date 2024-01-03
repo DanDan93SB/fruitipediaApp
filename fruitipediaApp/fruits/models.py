8977f9acd1d5a9083b4e2d6983315f39bc2bda94
DanDan93SB <122820568+DanDan93SB@users.noreply.github.com>
2024-01-03 20:28:18 +0200

    Assets - static

--- fruitipediaApp/fruits/models.py
+++ fruitipediaApp/fruits/models.py
@@ -1,3 +1,36 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from fruitipediaApp.fruits.validators import only_letters_validator
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Fruit(models.Model):
+    name = models.CharField(
+        max_length=30,
+        validators=[
+            MinLengthValidator(2),
+            only_letters_validator,
+        ],
+    )
+
+    Image_url = models.URLField()
+
+    description = models.TextField()
+
+    nutrition = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
