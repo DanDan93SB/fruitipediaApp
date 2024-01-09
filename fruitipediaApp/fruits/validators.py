35f38c1d6c8ef96579268984eb25929c052fdc2c
DanDan93SB <122820568+DanDan93SB@users.noreply.github.com>
2024-01-09 19:47:23 +0200

    Assets - static

--- fruitipediaApp/fruits/validators.py
+++ fruitipediaApp/fruits/validators.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def only_letters_validator(value):
-    if not value.isalpha():
+    if not value.replace(" ", "").isalpha():
         raise ValidationError("Fruit name should contain only letters!")
